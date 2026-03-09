4bafc7fe8ffb40551f84ca33abeb54ddf6a9f81a
pasinat0 <joaogabimelo@gmail.com>
2026-03-09 16:50:12 -0300

    adicionando jogador

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -4,4 +4,20 @@ Jogador = "X"
 
 
 
+def ExibeTabuleiro():
+    for linha in tabuleiro:
+        print(' | '.join(linha))
+        print('-' * 6)
+
+
+def jogada(linha,coluna):
+    tabuleiro[linha][coluna] = Jogador
+    if Jogador == 'X':
+      return 'O'
+    else:
+        return 'X'
+
+Jogador = jogada(1,1)
+Jogador = jogada(2,1)
+ExibeTabuleiro()
 
